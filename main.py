9cc8ab8f04f95b5a9f4f9d9ae1f95f01dfa03645
Murhaf-Mo <99115205+Murhaf-Mo@users.noreply.github.com>
2023-09-22 01:45:44 +0300

    innt

---
+++ main.py
@@ -0,0 +1,14 @@
+def calculate_points(gry):
+    total = 0
+    for i in range(10):
+        for j in range(10):
+            if gry[i][j] == 'X':
+                total += min(i + 1, j + 1, 10 - i, 10 - j)
+    return total
+
+
+t = int(input())
+for _ in range(t):
+    grid = [input() for _ in range(10)]
+    total_points = calculate_points(grid)
+    print(total_points)
